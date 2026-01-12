235c295fbcc589a2678c302a9aa88afdd1e07fb3
Mohammed Qasim <m261m@yahoo.com>
2026-01-12 05:00:23 +0300

    Add Christmas theme with animated snowflakes and festive styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div className="app">
-      <h1>hello world!</h1>
+      <div className="snowflakes">
+        {[...Array(50)].map((_, i) => (
+          <div key={i} className="snowflake">❄</div>
+        ))}
+      </div>
+      <h1 className="christmas-title">🎄 Merry Christmas! 🎄</h1>
+      <p className="christmas-subtitle">Happy Holidays from Hello World!</p>
     </div>
   )
 }
